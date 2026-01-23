c210b60c7ecf59673a7cf8dc824d032b448bcace
João Pedro Cordeiro Guimarães <jpcguim@gmail.com>
2026-01-23 00:56:15 -0300

    feat: initialize Spring Boot project with Maven configuration

---
+++ backend/src/test/java/br/com/fourzerofourdev/salesanalyticsbackend/SalesAnalyticsBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.fourzerofourdev.salesanalyticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAnalyticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
